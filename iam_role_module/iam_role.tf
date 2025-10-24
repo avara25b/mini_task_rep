4518945e0785194a317f6afaa4e8bc5e54075902
avara25b <andrii.var@edu.312school.com>
2025-10-24 12:07:15 -0400

    modified iam_role

--- iam_role_module/iam_role.tf
+++ iam_role_module/iam_role.tf
@@ -1,24 +1,25 @@
 resource "aws_iam_role" "admin_access_role" {
-
   name = "admin_access_role"
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
         Effect = "Allow"
+        Action = "sts:AssumeRole"
         Principal = {
-           AWS = "505789305843"
-        },
-        "Condition": {}
+          AWS = var.arn
+        }
       }
     ]
   })
 
   tags = {
     Name = "${var.name}-iam_role"
-    tag-key = "tag-value"
   }
+}
+
+resource "aws_iam_role_policy_attachment" "admin_access_attach" {
+  role       = aws_iam_role.admin_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
--- iam_role_module/vars.tf
+++ iam_role_module/vars.tf
@@ -2,3 +2,13 @@ variable "name" {
     type = string
     default = "mini-project"
 }
+
+variable "arn" {
+  description = "List of teammate AWS account ARNs allowed to assume this role"
+  type        = list(string)
+  default     = [
+    "arn:aws:iam::505789305843:role/RoleName", # teammate 1
+    "arn:aws:iam::123456789012:role/RoleName", # teammate 2
+    "arn:aws:iam::987654321098:role/RoleName"  # teammate 3
+  ]
+}
